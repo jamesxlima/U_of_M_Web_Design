6de4ea09968e4bea06833a980c9e17b0732e6c0f
James Lima <jamesxlima@gmail.com>
2022-12-04 22:02:15 -0500

    Added Week 4 JS HW

---
+++ JS Week 4/jsweek4hw.js
@@ -0,0 +1,11 @@
+function billingFunction(){
+    if(same.checked){
+      document.getElementById('billingName').value= document.getElementById('shippingName').value;
+      document.getElementById('billingZip').value=document.getElementById('shippingZip').value;
+    } 
+    
+    else {
+      document.getElementById('billingName').value="";
+      document.getElementById('billingZip').value="";
+    }
+  }
